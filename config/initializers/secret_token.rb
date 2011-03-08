59b5b2caa3a0dd2bd36c73e1b2431b600a9cb48d
Ronald <ronald@boshecker.nl>
2011-03-08 19:55:52 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chianti::Application.config.secret_token = 'aab84eb1044e372407b320563aac59294e188b650b470a1e3ed63c2d9cfb21a0070c3fd27f672c452f5113f564dadc83914a5562533de0c87fe3f2ee06f3bd20'
