51c9b58d68f750535723a1b4cb41e6aea5ef2421
peter de ruijter <peter@l2code.nl>
2011-03-30 21:39:23 +0200

    Added hamlpages and layouts. Stiertjes. <3

--- db/schema.rb
+++ db/schema.rb
@@ -24,27 +24,6 @@ ActiveRecord::Schema.define(:version => 20110319174639) do
     t.string   "url"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
-    t.string   "reset_password_token"
-    t.string   "remember_token"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
   create_table "wine_categories", :force => true do |t|
     t.string   "title"
     t.string   "url"
